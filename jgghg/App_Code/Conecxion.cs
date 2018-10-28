359555923e4821f166feafec527ebbc9ab1e5b69
Henry <Henry@LAPTOP-J5F8GEO9>
2018-10-28 15:49:49 -0600

    haha

--- jgghg/App_Code/Conecxion.cs
+++ jgghg/App_Code/Conecxion.cs
@@ -8,18 +8,48 @@ using System.Data.SqlClient;
 
 namespace jgghg.App_Code
 {
-    
-    public class Conecxion
+
+    public class Conexion
     {
         private static SqlConnection objConexion;
         private static string error;
 
 
-        public static SqlConnection getConexion() {
+        public static SqlConnection getConexion()
+        {
+
+            if (objConexion != null)
+            
+                return objConexion;
+                objConexion = new SqlConnection();
+                objConexion.ConnectionString = "Data Source=SQL7004.site4now.net;Initial Catalog=DB_A41A57_HenrySD;User Id=DB_A41A57_HenrySD_admin;Password=123456789LOPEZ";
+            
+            try
+            {
+                objConexion.Open();
+                return objConexion;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+                return null;
+
+            }
+
+
+        }
+
+        public static void cerrarConexion()
+        {
+
+            if (objConexion != null) {
+                objConexion.Close();
+            }
 
 
 
-            return null;
+
+
         }
     }
 }
