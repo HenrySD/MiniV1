d8470f049920f1d5de07de09b4a659f6e812bb74
Henry <Henry@LAPTOP-J5F8GEO9>
2018-10-28 15:18:45 -0600

    APPCODE

---
+++ jgghg/App_Code/Conecxion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace jgghg.App_Code
+{
+    
+    public class Conecxion
+    {
+        private static SqlConnection objConexion;
+        private static string error;
+
+
+        public static SqlConnection getConexion() {
+
+
+
+            return null;
+        }
+    }
+}
