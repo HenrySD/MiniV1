2622e43c2ba85ad741c86045b5a6df74fc7d9931
Henry <Henry@LAPTOP-J5F8GEO9>
2018-11-11 00:45:43 -0600

    Otra UPDATE Mejor

--- jgghg/ConexionSD.cs
+++ jgghg/ConexionSD.cs
@@ -14,6 +14,7 @@ namespace jgghg
         private SqlConnection conn ;
         private SqlCommand cmd;
         private String myConnectionString ;
+
         
         public ConexionSD()
         {
